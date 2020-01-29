890a02f9a75935c5679b12ec5a6d50da917556a8
Andrii Kovalenko <Andrii@invictusgurus.com>
2020-01-29 12:16:18 +0200

    Inited.

---
+++ tests/TauCode.Cqrs.Mq.Testing/CqrsMqTestingDummyFixture.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TauCode.Cqrs.Mq.Testing
+{
+    [TestFixture]
+    public class CqrsMqTestingDummyFixture
+    {
+        [Test]
+        public void CqrsMqTestingDummyTest()
+        {
+            Assert.Pass("Added for CI");
+        }
+    }
+}
