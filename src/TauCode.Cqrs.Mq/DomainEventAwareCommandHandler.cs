d9e825b98cc2115c703992d9dd389ae44192afa7
Andrey Kovalenko <ak@taucode.com>
2020-10-08 19:11:46 +0300

    Updated refs.

--- src/TauCode.Cqrs.Mq/DomainEventAwareCommandHandler.cs
+++ src/TauCode.Cqrs.Mq/DomainEventAwareCommandHandler.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using TauCode.Cqrs.Abstractions;
 using TauCode.Cqrs.Commands;
 using TauCode.Domain.Events;
 using TauCode.Mq;
